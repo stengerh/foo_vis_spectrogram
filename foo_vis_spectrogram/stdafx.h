5a0649fdf7f2e05cb01a1aef716f4ad1ba0ec4ae
Holger Stenger <holger.stenger@gmx.de>
2017-01-09 19:58:03 +0100

    Switch from helpers to ATLHelpers

--- foo_vis_spectrogram/stdafx.h
+++ foo_vis_spectrogram/stdafx.h
@@ -23,26 +23,8 @@
 #define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
 #endif
 
-#include <atlbase.h>
-//You may derive a class from CComModule and use it if you want to override
-//something, but do not change the name of _Module
-extern CComModule _Module;
-// ATL
-#include <atlcom.h>
-//#include <atlhost.h>
-#include <atlctl.h>
-#include <atlwin.h>
-
-// WTL
-#include <atlapp.h>
-//#include <atlframe.h>
-#include <atlcrack.h>
-#include <atlmisc.h>
-//#include <atldlgs.h>
-#include <atlctrls.h>
-
 #include <foobar2000/SDK/foobar2000.h>
-#include <foobar2000/helpers/helpers.h>
+#include <foobar2000/ATLHelpers/ATLHelpers.h>
 
 #undef BEGIN_MSG_MAP
 #define BEGIN_MSG_MAP(x) BEGIN_MSG_MAP_EX(x)
